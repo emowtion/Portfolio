b40078c85b57eeb33e74adf1d6c06ceb7675bfae
prycedom <prycedom@gmail.com>
2020-08-09 13:06:11 -0400

    Added js for exeeds

--- js/script.js
+++ js/script.js
@@ -14,54 +14,59 @@ let units = [
     url: "https://prycedom.github.io/responsive_layout_v3/",
     img: "img/UNIT 2 img.png",
     alt: "Mobile Phone",
-    title: "My Personal Profile Page",
-    description: "A simple site demonstrating HTML and CSS."
-  },
-  // UNIT 3
-  {
-    url: "https://prycedom.github.io/portfolio_update_v1/",
-    img: "img/UNIT 1 img.png",
-    alt: "Resume image",
     title: "Mobile-first Responsive Layout",
     description: "Created a website using CSS and mobile-first responsive layout."
   },
+  // UNIT 3
   {
-    url: "https://prycedom.github.io/portfolio_update_v1/",
-    img: "img/UNIT 1 img.png",
-    alt: "Resume image",
-    title: "My Personal Profile Page",
-    description: "A simple site demonstrating HTML and CSS."
+    url: "https://prycedom.github.io/Unit-3-Online-Registration_Form/",
+    img: "img/UNIT 3 img.png",
+    alt: "Registration Form",
+    title: "Online Registration Form",
+    description: "Created a registration form using CSS, HTML tables, html forms, CSS flexbox."
   },
+  // UNIT 4
   {
-    url: "https://prycedom.github.io/portfolio_update_v1/",
-    img: "img/UNIT 1 img.png",
-    alt: "Resume image",
-    title: "My Personal Profile Page",
-    description: "A simple site demonstrating HTML and CSS."
+    url: "https://prycedom.github.io/Unit-4/",
+    img: "img/UNIT 4 img.png",
+    alt: "Style Guide",
+    title: "Web Style Guide",
+    description: "Created a Style Guide using transitions and transforms, Sass, HTML, and CSS."
   },
+  // UNIT 5
   {
-    url: "https://prycedom.github.io/portfolio_update_v1/",
-    img: "img/UNIT 1 img.png",
-    alt: "Resume image",
-    title: "My Personal Profile Page",
-    description: "A simple site demonstrating HTML and CSS."
+    url: "https://prycedom.github.io/Unit-5/",
+    img: "img/UNIT 5 img.png",
+    alt: "Photo Album",
+    title: "Interactive Photo Gallery",
+    description: "Created an interactive photo gallery using HTML CSS, and JavaScript."
   },
+  // UNIT 6
   {
-    url: "https://prycedom.github.io/portfolio_update_v1/",
-    img: "img/UNIT 1 img.png",
-    alt: "Resume image",
-    title: "My Personal Profile Page",
-    description: "A simple site demonstrating HTML and CSS."
+    url: "https://prycedom.github.io/Unit-6/",
+    img: "img/UNIT 6 img.png",
+    alt: "Game Show",
+    title: "Game Show App",
+    description: "Created an interactive photo gallery using HTML CSS, and JavaScript."
   },
+  // UNIT 7
   {
-    url: "https://prycedom.github.io/portfolio_update_v1/",
-    img: "img/UNIT 1 img.png",
-    alt: "Resume image",
-    title: "My Personal Profile Page",
-    description: "A simple site demonstrating HTML and CSS."
+    url: "https://prycedom.github.io/Unit-7/",
+    img: "img/UNIT 7 img.png",
+    alt: "Dashboard",
+    title: "WebApp Dashboard",
+    description: "Created an interactive dashboard for a web application using SVG graphics and JavaScript creating interactive tables, charts, and graphics."
+  },
+  // UNIT 8
+  {
+    url: "https://prycedom.github.io/Unit-8/",
+    img: "img/UNIT 8 img.png",
+    alt: "Directory",
+    title: "Employee Directory",
+    description: "Created an interactive photo gallery using HTML CSS, and JavaScript."
   }
 ];
-// Units/Cards
+// Unit-Cards
 units.forEach(unit => {
   let unitHtml = `<a href="${unit.url}" target="_blank" class="card col-sm-6 col-lg-3 border p-3 mb-2 bg-light text-dark"> 
                   <img src="${unit.img}" class="card-img-top" alt="${unit.alt}">
